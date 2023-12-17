4a1894440f9b4e3c40e8a73ad848c37834c64b00
Marco Upia <markremembers@gmail.com>
2023-12-16 23:57:48 -0500

    need to revist

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,32 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [spicy['name'] for spicy in spicy_foods ]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = []
+    for spicy in spicy_foods:
+        if spicy['heat_level'] > 5:
+            spiciest_foods.append(spicy)
+    return spiciest_foods
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for spicy in spicy_foods:
+        print(f'{spicy["name"]} ({spicy["cuisine"]}) | Heat Level: {"🌶" * spicy["heat_level"]}')
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for spicy in spicy_foods:
+            if spicy['cuisine'] == cuisine:
+                return spicy
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
+    
 
 def get_average_heat_level(spicy_foods):
-    pass
+    return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
